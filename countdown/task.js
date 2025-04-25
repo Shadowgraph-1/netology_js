8d75a59b2da30dacc3e831e78d84dc3882489357
Shadowgraph <litvin4chuk@mail.ru>
2025-04-25 16:32:40 +0500

    first commit countdown

---
+++ countdown/task.js
@@ -0,0 +1,12 @@
+let timerElement = document.getElementById('timer');
+let seconds = parseInt(timerElement.textContent);
+
+const countdown = setInterval(() => {
+    seconds--;
+    timerElement.textContent = seconds;
+    
+    if (seconds <= 0) {
+        clearInterval(countdown);
+        alert('Вы победили в конкурсе!');
+    }
+}, 1000);
